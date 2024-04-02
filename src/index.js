84cffc8878c71590119132fa2a9b41c09ab6511b
Flávio Filho <flavio.vezono@ufu.br>
2024-04-02 11:13:52 -0300

    header type

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ const {pptx, path} = await generateSlides();
 app.get("/", (req, res) => {
     pptx.stream()
         .then((data) => {
+            res.setHeader("Content-Type", "application/vnd.openxmlformats-officedocument.presentationml.presentation");
             res.writeHead(200, { "Content-disposition": "attachment;filename=" + path, "Content-Length": data.length });
             res.end(new Buffer.alloc(data.byteLength, data, "binary"));
         })
